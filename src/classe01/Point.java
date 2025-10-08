85f664cfcf9ef92c3746b7d2df99d76bee5b0040
adediu <adediu@autonoma.pt>
2025-10-08 16:55:21 +0100

    Employee e Lowyer

---
+++ src/classe01/Point.java
@@ -0,0 +1,27 @@
+package classe01;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void translate(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point(3, 4);
+        p.translate(1, 2);
+        System.out.println(p);   // (4, 6)
+    }
+}
+
